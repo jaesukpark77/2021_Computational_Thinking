cac291aa02d3c8092b2e2eac73e10d129599aa74
jaesukpark77 <jaesukpark77@naver.com>
2021-06-24 13:58:37 +0900

    [3주차] 번역프로그램

---
+++ [3주차] 번역프로그램.py
@@ -0,0 +1,15 @@
+#번역 프로그램
+
+import webbrowser
+
+q = input("궁금한거?")
+webbrowser.open("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=" + q)
+
+
+#번역기
+import webbrowser
+
+q = input("궁금한거?")
+webbrowser.open("https://translate.google.com/?sl=auto&tl=en&text="+q)
+
+
