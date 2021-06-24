e3623367aea935ba68e861fd1c601e2d35773634
jaesukpark77 <jaesukpark77@naver.com>
2021-06-24 13:58:47 +0900

    [3주차] 출력함수

---
+++ [3주차] 출력함수.py
@@ -0,0 +1,16 @@
+#p.59
+
+import time
+import winsound     #삑 소리 난다
+
+winsound.Beep(600, 500)
+print('hello')
+time.sleep(1)
+
+winsound.Beep(600, 500)
+print('gildong')
+time.sleep(1)
+
+winsound.Beep(600, 500)
+print('study python')
+time.sleep(1)
