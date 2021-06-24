5874477ee1d963d88e2d0b9ef43895aef1c89cb2
jaesukpark77 <jaesukpark77@naver.com>
2021-06-24 13:58:22 +0900

    [3주차] input함수

---
+++ [3주차] input함수.py
@@ -0,0 +1,23 @@
+#p.86 input 함수
+
+print("이름 입력 : " , end = " ")
+name = input()
+print("안녕~", name, "씨 반가워요")
+
+
+#p. 87 프롬프트 변경
+
+print("당신의 고향은? ", end = " ")
+home = input()
+print("아름다운" , home, "출신이군요")
+
+home = input("당신의 고향은?  ")
+print("아름다운" , home, "출신이군요")
+
+#p.90 예제2
+
+color = input("무슨 색을 좋아해요? ")
+
+animal = input("어떤 동물을 좋아해요? ")
+
+print("그럼", color, animal, "를 좋아하겠군요")
