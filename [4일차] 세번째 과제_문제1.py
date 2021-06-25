fdb2a599f4060c78b7faed92b4968f309e948789
jaesukpark77 <jaesukpark77@naver.com>
2021-06-25 16:52:07 +0900

    [4일차] 세번째 과제_문제1

---
+++ [4일차] 세번째 과제_문제1.py
@@ -0,0 +1,18 @@
+#세번째 과제 문제 1번
+
+print("안녕하세요.")
+print("나는 인공지능 채팅 로봇입니다.")
+name = input("당신의 이름은 무엇인가요?\n")
+
+print("%s씨 만나서 반가워요" %name)
+location = input("당신의 고향은 어딘가요?\n")
+
+print("%s씨는 아름다운 %s에서 왔군요." %(name, location))
+age = int(input("당신의 나이는 몇살인가요?\n"))
+
+print("%s씨는 내년이면 %d살이군요. 벌써 %d살이시군요. 세월 참 빠르네요." %(name, age+1, age))
+print("안녕 %s씨, 다음에 또 만나요." %name)
+
+
+
+
