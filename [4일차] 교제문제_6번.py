31b32b8908f860ba856ad5be0698fd757f9d8c64
jaesukpark77 <jaesukpark77@naver.com>
2021-06-25 16:51:42 +0900

    [4일차] 교제문제_6번

---
+++ [4일차] 교제문제_6번.py
@@ -0,0 +1,18 @@
+#6번
+
+n = int(input("정수 입력?"))
+
+if n%2==0:
+    print("%d는 2의 배수" %n)
+else:
+    print("%d는 2의 배수 아님" %n)
+    
+if n%3==0:
+    print("%d는 3의 배수" %n)
+else:
+    print("%d는 3의 배수 아님" %n)
+    
+if n%5==0:
+    print("%d는 5의 배수" %n)
+else:
+    print("%d는 5의 배수 아님" %n)
