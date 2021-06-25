7c4c0a18e795c7327725ea4fd9185429dbc7b9bf
jaesukpark77 <jaesukpark77@naver.com>
2021-06-25 11:23:43 +0900

    [3일차] 사칙연산

---
+++ [3일차] 사칙연산.py
@@ -0,0 +1,9 @@
+#p. 138 사칙연산
+
+a = 10
+b = 3
+
+print("%4d + % 4d = %4d" %(a, b, a+b))
+print("%4d - % 4d = %4d" %(a, b, a-b))
+print("%4d * % 4d = %4d" %(a, b, a*b))
+print("%4d / % 4d = %4.2f" %(a, b, a/b))
